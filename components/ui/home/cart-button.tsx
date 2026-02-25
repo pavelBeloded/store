0655aaca193d087da32d920f3bc685c835c99e77
PavelBeloded <beloded_2007@bk.ru>
2026-02-26 00:03:13 +0300

    feat: cart items count badge

--- components/ui/home/cart-button.tsx
+++ components/ui/home/cart-button.tsx
@@ -4,18 +4,25 @@ import { ShoppingCart } from "lucide-react";
 import { Button } from "../button";
 import { Badge } from "../badge";
 import Link from "next/link";
+import { useCartStore } from "@/app/store/use-cart-store";
 
 export default function CartButton() {
+  const items = useCartStore((state) => state.items);
+
+  const totalItems = items.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
     <Link href="/cart">
       <Button>
         <ShoppingCart />
-        <Badge
-          variant="destructive"
-          className="absolute -top-1 -right-1 h-5 w-5 flex items-center justify-center rounded-full p-0 text-xs"
-        >
-          3
-        </Badge>
+        {totalItems === 0 || (
+          <Badge
+            variant="destructive"
+            className="absolute -top-1 -right-1 h-5 w-5 flex items-center justify-center rounded-full p-0 text-xs"
+          >
+            {totalItems}
+          </Badge>
+        )}
       </Button>
     </Link>
   );
